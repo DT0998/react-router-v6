8f8226071f4fd19447733518004c969544d3cf9e
DT0998 <duongkytrieuvn@gmail.com>
2022-06-20 15:19:49 +0700

    update react router redirect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import {Route} from 'react-router-dom'
-import MainHeader from './components/MainHeader';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
+import { Route, Switch,Redirect } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Welcome from "./pages/Welcome";
 function App() {
   return (
     <div>
-      <header>
-      <MainHeader/>
-      </header>
+      <MainHeader />
       <main>
-      <Route path="/welcome" >
-        <Welcome/>
-      </Route>
-      <Route path="/products">
-        <Products/>
-      </Route>
+        <Switch>
+          <Route exact path="/" >
+            <Redirect to="/welcome"/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,13 @@
+import { Route } from "react-router-dom";
+
 function Welcome() {
     return (  
-        <h1>The Welcome page</h1>
+        <section>
+            <h1>The Welcome page</h1>
+            <Route path="/welcome/new-user">
+                <p>Welcome,new user!</p>
+            </Route>
+        </section>
     );
 }
 
